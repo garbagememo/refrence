870873495b25747d198e2ff04d1eddd340721add
garbagememo <66831242+garbagememo@users.noreply.github.com>
2026-01-11 20:06:02 +0900

    Add files via upload

--- init.el
+++ init.el
@@ -1,33 +1,28 @@
-
-(setq scroll-step 1)  ; スクロールは 1 行ずつ
-(setq scroll-preserve-screen-position t)  ; ポイント位置を保ったままスクロール
-
-(define-key key-translation-map (kbd "C-h") (kbd "<DEL>"))
-
-(setq inhibit-startup-message t)
-(setq initial-scratch-message "")
-(setq inhibit-message "")
-
-(global-display-line-numbers-mode)
+;;; -*- lexical-binding: t -*-
  (autoload 'opascal-mode "opascal")
  (add-to-list 'auto-mode-alist
               '("\\.\\(pas\\|dpr\\|dpk\\)\\'" . opascal-mode))
-
-
-
+(setq tab-width 4)
+(setq indent-tabs-mode nil)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(cua-mode t nil (cua-base))
+ '(column-number-mode t)
+ '(cua-mode t)
+ '(current-language-environment "UTF-8")
  '(global-display-line-numbers-mode t)
- '(opascal-case-label-indent 2)
- '(opascal-indent-level 2)
+ '(indent-tabs-mode nil)
+ '(inhibit-startup-screen t)
+ '(package-selected-packages '(rust-mode))
  '(tab-bar-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "HackGen Console" :foundry "outline" :slant normal :weight regular :height 120 :width normal)))))
+;;ウインドウサイズの指定
+(set-frame-size (selected-frame) 120 40)
+
